210529c0cfdee9333fbc50bef01a784bd28f9457
Marco-Tulio27 <marcotulio2000@live.com>
2023-09-25 23:46:48 -0300

    firstupdt

--- main.py
+++ main.py
@@ -2,10 +2,28 @@
 # vamos definir funções para download, de low,medium e high quality 
 
 from pytube import YouTube
+class video:
+    def __init__(self):
+        self.link=input('Paste link from video here:  ')
+        self.url=self.link
+        print('Selecione a sua opção de download: \n alta definição 1 \n baixa resolução 2 ')
+        self.resolucao= input('Opção: ') 
+    def download(self):   
+        if self.resolucao == '1':
+            video= YouTube(self.url)
+            stream= video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
+            stream.download()
+        if self.resolucao == '2':
+            video=YouTube(self.url)
+            stream=video.streams.first().download()
+            stream.download()
+    
+baixar = video()
+baixar.download()
 
-link=input('Paste link from video here:  ')
-url=link
-video= YouTube(url)
-#stream= video.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first().download()
-stream=video.streams.get_highest_resolution()
-#print(str(stream))
+# MP4 (H.264)
+# WEBM (VP9)
+# 3GP
+# FLV
+
+#https://www.youtube.com/watch?v=_TV7MUfZZNs
